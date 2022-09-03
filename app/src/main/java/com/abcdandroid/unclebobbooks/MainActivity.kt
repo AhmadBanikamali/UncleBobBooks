d36c6f93997bc44d29c3bd326523709a656e605f
ahmad.banikamali1 <ahmad.banikamali.git@gmail.com>
2022-09-03 17:00:21 +0430

    make modules and inital libs (hilt and compose)

---
+++ app/src/main/java/com/abcdandroid/unclebobbooks/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.abcdandroid.unclebobbooks
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.abcdandroid.unclebobbooks.ui.theme.UncleBobBooksTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UncleBobBooksTheme {
+                // A surface container using the 'background' color from the theme
+                
+                // https://medium.com/swlh/clean-architecture-in-android-a-beginner-approach-be0ce00d806b
+                // https://www.googleapis.com/books/v1/volumes?q=Robert%20C.%20Martin
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    UncleBobBooksTheme {
+        Greeting("Android")
+    }
+}
---
+++ domain/src/main/java/com/abcdandroid/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.abcdandroid.domain
+
+class MyClass {
+}
